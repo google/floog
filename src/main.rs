740bbe1d0342658ed523dbf117b5aa24de03419b
Daniel Hackney <dan@haxney.org>
2018-12-02 00:08:38 -0800

    Some experiments in reading zip file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate zip;
+
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", args[0]);
+        return;
+    }
+
+    let fname = std::path::Path::new(&*args[1]);
+    let file = fs::File::open(&fname).unwrap();
+
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i);
+        print_file(file);
+    }
+}
+
+fn print_file<'a>(
+    in_file: zip::result::ZipResult<zip::read::ZipFile>,
+) -> zip::result::ZipResult<()> {
+    let file = in_file?;
+    println!("read file: {:?}", file.sanitized_name());
+    return Ok(());
 }
